3ef15279e1d897aab2120766039d2adef5588a98
root <root@debian.local>
2014-01-20 20:04:35 -0500

    built on linux32 with GCC 4.4.5; fixed build script

--- csrc/clipper/build-linux32.sh
+++ csrc/clipper/build-linux32.sh
@@ -5,4 +5,4 @@ g++ $CXXFLAGS clipper.c -shared -o ../../bin/linux32/libclipper.so -I. -L.
 rm -f libstdc++.a
 
 # check that libstdc++ is not a dependency
-ldd ../../linux/bin/libclipper.so
+ldd ../../bin/linux32/libclipper.so | grep libstdc++
