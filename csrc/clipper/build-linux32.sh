5c26a8ff816edf09eb535b86aa409b70ca13f294
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-30 16:35:10 +0000

    linux64 build

--- csrc/clipper/build-linux32.sh
+++ csrc/clipper/build-linux32.sh
@@ -1,8 +1,1 @@
-ln -sf "$(g++ -print-file-name=libstdc++.a)"
-
 g++ -O2 -s -static-libgcc -static-libstdc++ clipper.c -shared -o ../../bin/linux32/libclipper.so -I. -L.
-
-rm -f libstdc++.a
-
-# check that libstdc++ is not a dependency
-ldd ../../bin/linux32/libclipper.so | grep libstdc++
---
+++ csrc/clipper/build-linux64.sh
@@ -0,0 +1,1 @@
+g++ -O2 -s -static-libgcc -static-libstdc++ -fPIC clipper.c -shared -o ../../bin/linux64/libclipper.so -I. -L.
