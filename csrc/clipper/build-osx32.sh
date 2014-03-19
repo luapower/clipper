6e5ada41b074280c9c63a8051a9357d63bd7ccd0
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:33:08 +0200

    osx32 build

---
+++ csrc/clipper/build-osx32.sh
@@ -0,0 +1,1 @@
+g++ -arch i386 -mmacosx-version-min=10.4 -O2 clipper.c -shared -o ../../bin/osx32/libclipper.dylib -I. -L.
--- csrc/clipper/build-osx64.sh
+++ csrc/clipper/build-osx64.sh
@@ -1,1 +1,1 @@
-g++ -O2 -static-libstdc++ clipper.c -shared -o ../../bin/osx64/libclipper.dylib -I. -L.
+g++ -arch x86_64 -mmacosx-version-min=10.4 -O2 clipper.c -shared -o ../../bin/osx64/libclipper.dylib -I. -L.
