fb166d5823fa184d0896797aa5167686814ece6f
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:06:28 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/clipper/build-osx32.sh
+++ csrc/clipper/build-osx32.sh
@@ -1,1 +1,1 @@
-g++ -arch i386 -mmacosx-version-min=10.4 -O2 clipper.c -shared -o ../../bin/osx32/libclipper.dylib -I. -L.
+g++ -arch i386 -mmacosx-version-min=10.4 -O2 clipper.c -shared -install_name @loader_path/libclipper.dylib -o ../../bin/osx32/libclipper.dylib -I. -L.
--- csrc/clipper/build-osx64.sh
+++ csrc/clipper/build-osx64.sh
@@ -1,1 +1,1 @@
-g++ -arch x86_64 -mmacosx-version-min=10.4 -O2 clipper.c -shared -o ../../bin/osx64/libclipper.dylib -I. -L.
+g++ -arch x86_64 -mmacosx-version-min=10.4 -O2 clipper.c -shared -install_name @loader_path/libclipper.dylib -o ../../bin/osx64/libclipper.dylib -I. -L.
