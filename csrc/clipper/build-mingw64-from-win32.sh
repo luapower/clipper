410cc6931915253ebfb48a3394483dbe673486fa
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-05-05 02:24:44 +0300

    mingw64 static build of winpthread

--- csrc/clipper/build-mingw64-from-win32.sh
+++ csrc/clipper/build-mingw64-from-win32.sh
@@ -1,1 +1,1 @@
-i686-w64-mingw32-g++ -m64 -O2 -s -static-libgcc -static-libstdc++ clipper.c -shared -o ../../bin/mingw64/clipper.dll -I. -L.
+i686-w64-mingw32-g++ -m64 -O2 -s -static -static-libgcc -static-libstdc++ clipper.c -shared -o ../../bin/mingw64/clipper.dll -I. -L.
--- csrc/clipper/build-mingw64.sh
+++ csrc/clipper/build-mingw64.sh
@@ -1,1 +1,1 @@
-g++ -O2 -s -static-libgcc -static-libstdc++ clipper.c -shared -o ../../bin/mingw64/clipper.dll -I. -L.
+g++ -O2 -s -static -static-libgcc -static-libstdc++ clipper.c -shared -o ../../bin/mingw64/clipper.dll -I. -L.
